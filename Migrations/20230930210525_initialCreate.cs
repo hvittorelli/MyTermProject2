40565e6bff6bafe636561fb561509a366dd0b93e
hvittorelli <misshopev@gmail.com>
2023-09-30 19:03:48 -0400

    Add project files.

---
+++ Migrations/20230930210525_initialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTermProject.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    BookFormat = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "ID", "AuthorName", "BookFormat", "BookTitle", "Genre" },
+                values: new object[] { 1, "JK Rowling", 2, "Harry Potter and the Sorcerer's Stone", "Fantasy" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "ID", "AuthorName", "BookFormat", "BookTitle", "Genre" },
+                values: new object[] { 2, "James Clear", 0, "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones", "Self-Help" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "ID", "AuthorName", "BookFormat", "BookTitle", "Genre" },
+                values: new object[] { 3, "Kristin Hannah", null, "Night Road", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ Models/CatalogContext.cs
@@ -0,0 +1,45 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace MyTermProject.Models
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+        public DbSet<Books> Books { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>().HasData(
+
+                new Books
+                {
+                    ID = 1,
+                    BookTitle = "Harry Potter and the Sorcerer's Stone",
+                    AuthorName = "JK Rowling",
+                    Genre = "Fantasy",
+                    BookFormat = Format.Audio
+                },
+                new Books { 
+                    ID = 2,
+                    BookTitle = "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+                    AuthorName = "James Clear",
+                    Genre = "Self-Help",
+                    BookFormat = Format.Hardcover
+                 },
+                new Books
+                {
+                    ID = 3,
+                    BookTitle = "Night Road",
+                    AuthorName = "Kristin Hannah",
+                }
+            );
+        }
+    }
+}
